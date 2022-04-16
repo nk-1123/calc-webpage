1381139a9bd05948b1c10cfeabd4aeb8be54863c
nikita <nk01119888@gmail.com>
2022-04-16 12:58:31 +0530

    theme 1

---
+++ index.js
@@ -0,0 +1,26 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+screenValue='';
+for(item of buttons){
+    item.addEventListener('click', (e) =>{
+        buttonText= e.target.innerText;
+        
+        if(buttonText=='RESET'){
+            screenValue="";
+            screen.value= screenValue;
+        }
+        else if(buttonText=='='){
+            screen.value=eval(screenValue);
+        }
+        else if(buttonText=='DEL'){
+            screenValue= screenValue.slice(0,-1);
+            screen.value= screenValue;
+        }
+        else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+        
+    })
+}
